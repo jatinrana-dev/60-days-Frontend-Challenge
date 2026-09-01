d84eb177b344d4b4dc8efe8656efb7223471f8f8
Jatin Rana <jatinrana.code@gmail.com>
2026-09-01 16:10:31 +0530

    Added Components Practice

--- React/day1/01-jsx-props-states/src/App.jsx
+++ React/day1/01-jsx-props-states/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Add from './components/Add'
+
 
 const App = () => {
   return (
     <div>
       <h1>heyyyyy i am react </h1>
+      <Add />
     </div>
+   
   )
 }
 
---
+++ React/day2/02-basic-practice/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <div>
+      <h2>Main content</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repudiandae perferendis facilis molestiae, culpa quisquam explicabo quasi earum commodi necessitatibus veniam rem maiores cupiditate numquam consequatur delectus. Nobis totam est impedit!</p>
+    </div>
+  )
+}
+
+export default MainContent
